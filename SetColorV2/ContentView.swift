785b45dc1b5873e18234d79f2398fee3746608f4
KremnevViacheslav <rusted2192@gmail.com>
2022-04-19 20:25:34 +0300

    Initial Commit

---
+++ SetColorV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SetColorV2
+//
+//  Created by Вячеслав Кремнев on 19.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SetColorV2/SetColorV2App.swift
@@ -0,0 +1,17 @@
+//
+//  SetColorV2App.swift
+//  SetColorV2
+//
+//  Created by Вячеслав Кремнев on 19.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SetColorV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
